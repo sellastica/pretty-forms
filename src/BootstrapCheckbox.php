4630f1e6bea5331c8531c0f6e4e4468e503bc44a
Tomas Krejci <tomas@differencemakers.cz>
2019-05-03 09:03:19 +0200

    .

--- src/BootstrapCheckbox.php
+++ src/BootstrapCheckbox.php
@@ -1,7 +1,6 @@
 <?php
 namespace Sellastica\PrettyForms;
 
-use Nette\Forms\Controls\BaseControl;
 use Nette\Forms\Controls\Checkbox;
 use Nette\Utils\Html;
 
@@ -9,15 +8,31 @@ class BootstrapCheckbox extends Checkbox
 {
 	/** @var string|null */
 	private $labelClass;
+	/** @var string|null */
+	private $tooltipTitle;
+	/** @var string|null */
+	private $tooltipDescription;
 
 
 	/**
 	 * @param null|string $class
 	 * @return $this
 	 */
-	public function setLabelClass(?string $class)
+	public function setLabelClass(?string $class): BootstrapCheckbox
 	{
 		$this->labelClass = $class;
+		return $this;
+	}
+
+	/**
+	 * @param string $title
+	 * @param string $description
+	 * @return BootstrapCheckbox
+	 */
+	public function setTooltip(string $title, string $description): BootstrapCheckbox
+	{
+		$this->tooltipTitle = $title;
+		$this->tooltipDescription = $description;
 		return $this;
 	}
 
@@ -30,6 +45,17 @@ class BootstrapCheckbox extends Checkbox
 		$el = Html::el('div')->setAttribute('class', 'checkbox ' . ($this->labelClass ?? 'checkbox-primary'));
 		$el->insert(0, $this->getControlPart());
 		$el->insert(1, $this->getLabelPart());
+
+		if (isset($this->tooltipTitle)
+			&& isset($this->tooltipDescription)) {
+			$el->insert(2, Html::el('i')
+				->setAttribute('class', 'fa fa-question-circle advanced-tooltip ml-5')
+				->data('original-title', $this->tooltipTitle)
+				->data('content', $this->tooltipDescription)
+				->data('toggle', 'popover')
+				->data('container', 'body')
+			);
+		}
 
 		return $el;
 	}
